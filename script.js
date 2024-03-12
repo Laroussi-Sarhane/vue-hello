f19bb82bc2dd27d2718b9a0bf96623afc08257ef
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-12 10:03:18 +0100

    add img

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ createApp({
   // i dati vengono ggestiti dal metodo data 
   // DATA è un metodo che restituisce un oggetto 
   // questo oggetto contterra tutte le nostre variabili che in realta sono proprieta dell oggetto data 
+
+  //i metodi sono le nostre funzioni => è un oggetto che contiene le nostre funzioni 
   data(){
     return{
       messaggio: 'ciao vue vediamo cosa succede ',
@@ -13,10 +15,24 @@ createApp({
       eta: '23',
       testoHtml: `<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quod fugit facere repellendus tempore enim sit dolorum modi recusandae dicta. Dignissimos porro quisquam totam omnis voluptatum quos quam odio veritatis?</p>`,
       miaClasseCss: 'verde',
+      contatore: 0 
       
       
 
     }
+  },
+
+   methods:{
+    saluto(){
+      console.log(ciao);
+    },
+    incrementoContatore(){
+      // per accedere ai data dai methods devo usare la parola chiave this
+      this.contatore++;
+      console.log('incremento il contatore', this.contatore)
+
+    }
+
   }
 
 }).mount('#app')
